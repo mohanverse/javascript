77ccdd3e99ce1be181dacd3d06e3c12a11de1c30
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2022-09-11 22:47:12 +0530

    mohanverse

--- higherorderfunc.js
+++ higherorderfunc.js
@@ -2,11 +2,37 @@
 // mini program
 
 // call back fuction
-const callback1 = (n) => {
-    return n ** 2;
-};
+// const callback1 = (n) => {
+//     return n ** 2;
+// };
 // function to multiple sqaure with the number
-function cube(callback1, n){
-    return callback1(n) * n;
-} 
-console.log(cube(callback1, 5));
+// function cube(callback1, n){
+//     return callback1(n) * n;
+// } 
+// console.log(cube(callback1, 5));
+
+// setTimeout
+
+// function couse(){
+//     console.log("welcome to the full stack javascript couse bootcamp");
+// }
+// setTimeout(couse, 3000);
+
+// setTimeout(() => {
+//     console.log("welcome to the full stack javascript couse bootcamp");
+// },3000);
+
+
+
+//setInterval
+// setInterval(() => {
+//     console.log("hello");
+// }, 300);
+
+//for each
+// let arr = ["Hey", "hi", "hello", "namste"];
+// arr.forEach((val) =>{
+//     console.log(val);
+// });
+
+//map
