f52186d18691be9b1a8d7b2aa055985c93e4bb4c
Mohan Chaudhary <MOHANVERSE@GMAIL.COM>
2022-09-11 11:21:24 +0530

    mohanverse

--- object.js
+++ object.js
@@ -2,24 +2,48 @@
 //TODO: OBJECT
 
 // method in object
-let userDetailCourse = {
+// let userDetailCourse = {
+//     firstName: "Mohan",
+//     lastName: "Chaudhary",
+//     role: "Student",
+//     googleLogin: true,
+//     courseList: [],
+//     buyCorse: function (courseName){
+//     this.courseList.push(courseName);
+//     },
+
+//     getCourseCount: function(){
+//         return `${userDetailCourse.firstName} is enrolled ${this.courseList.length} coursea`;
+//     },
+// }
+
+
+// console.log(userDetailCourse.firstName);
+// userDetailCourse.buyCorse("Angular.js");
+// console.log(userDetailCourse.courseList);
+// userDetailCourse.buyCorse("react js");
+// console.log(userDetailCourse.courseList);
+// console.log(userDetailCourse.getCourseCount());
+
+// //keys
+// console.log(Object.keys(userDetailCourse));
+// //values
+// console.log(Object.values(userDetailCourse));
+
+// //cheaking key
+// console.log(userDetailCourse.hasOwnProperty("firstName"));
+
+
+
+let userDetails = {
     firstName: "Mohan",
     lastName: "Chaudhary",
     role: "Student",
-    googleLogin: true,
-    courseList: [],
-    buyCorse: function (courseName){
-    this.courseList.push(courseName);
-    },
+    loginCount: 25,
+};
 
-    getCourseCount: function(){
-        return `${userDetailCourse.firstName} is enrolled ${this.courseList.length} coursea`;
-    },
+for(let x in userDetails){
+    //console.log(x);
+    //console.log(userDetails[x]);
+    console.log(`key is ${x} and values is ${userDetails[x]}`);
 }
-
-console.log(userDetailCourse.firstName);
-userDetailCourse.buyCorse("Angular.js");
-console.log(userDetailCourse.courseList);
-userDetailCourse.buyCorse("react js");
-console.log(userDetailCourse.courseList);
-console.log(userDetailCourse.getCourseCount());
