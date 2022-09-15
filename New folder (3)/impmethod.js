92399ea4976b017cb1b01f169e111e06a814992c
Mohan Kumar Chaudhary <98375650+mohanverse@users.noreply.github.com>
2022-09-15 20:30:28 +0530

    mohanverse

---
+++ New folder (3)/impmethod.js
@@ -0,0 +1,53 @@
+// important array methods
+
+// const numbers = [4,2,5,8];
+// function multwo(number, index){
+//     console.log("index is ", index);
+//     console.log(`${number}*2=${number*2}`);
+// }
+// numbers.forEach(multwo);
+// for(let i = 0; i<numbers.length; i++){
+//     multwo(numbers[i],i);
+// } 
+
+
+
+//map method
+// const users = [
+//     {firstName: "mohanverse", age:23},
+//     {firstName: "shubham", age:19},
+//     {firstName: "shubhash", age:18},
+//     {firstName: "mohan", age:20},
+// ]
+
+// const userName = users.map((user)=>{
+//     return user.firstName;
+// });
+// console.log(userName);
+
+// filter method
+
+// const store = [1,2,3,4,5,6];
+// const isEven = function(number){
+//     return number%2==0;
+// }
+// const evenNumbers = store.filter(isEven);
+// console.log(evenNumbers);
+
+//reduse method
+// const array1 = [
+//     {prodectId: 1, productName: "mobile", Price: 15000},
+//     {prodectId: 2, productName: "tv", Price: 15000},
+//     {prodectId: 3, productName: "laptop", Price: 13000},
+// ];
+// const sumWithInitial = array1.reduce(
+//   (previousValue, currentValue) => {
+//     return  previousValue + currentValue.Price
+// },0);
+
+// console.log(sumWithInitial);
+
+
+
+
+
